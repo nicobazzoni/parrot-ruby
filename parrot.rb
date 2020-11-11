f0c8a094463e4a3216db38ad8393ee30e016caf0
Nico Bazzoni <you@example.com>
2020-11-10 20:07:22 -0500

    commit tree 6cd3221eb65d3139d3197576be68fa8fbf816048

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,17 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(name = nil) 
+    if name 
+        puts name
+        return name
+    else 
+         puts "Squawk!"
+        return "Squawk!"
+    end
+end 
+
+
+
+
+
+
